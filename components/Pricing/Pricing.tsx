02239bfe6fd1cc51f65fdd5312b27239a17373e8
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2024-09-23 19:05:32 +0545

    fix: smaller screen

--- components/Pricing/Pricing.tsx
+++ components/Pricing/Pricing.tsx
@@ -155,15 +155,15 @@ export const PricingComponent = () => {
 
 export function FeatureComparisionTable({planFeatures}) {
     return (
-      <div className="mt-24">
+      <div className="mt-24 w-full overflow-x-auto text-xs sm:text-base">
         <table className="min-w-full border border-gray-200 rounded-lg">
           <thead className='border-b border-gray-200'>
             <tr>
-              <th className="w-[200px] py-3 px-4 text-left capitalize">{planFeatures.type} Types</th>
+              <th className="w-[100px] sm:w-[200px] py-1.5 sm:py-3 px-2 sm:px-4 text-left capitalize">{planFeatures.type} Types</th>
               {planFeatures.plans.map((plan) => (
                 <th
                   key={plan.name}
-                  className={"py-3 px-4 text-center"}
+                  className="py-1.5 px-2 sm:py-3 sm:px-4 text-center"
                 >
                   {plan.name}
                 </th>
@@ -174,8 +174,8 @@ export function FeatureComparisionTable({planFeatures}) {
             {planFeatures.types.map((category) => (
               <React.Fragment key={category.category}>
                 <tr className="bg-gray-50 border-b border-gray-200">
-                  <td className="font-semibold py-3 px-4" colSpan={3}>
-                   {category.category}
+                  <td className="font-semibold py-1.5 px-2 sm:py-3 sm:px-4" colSpan={planFeatures.plans.length + 1}>
+                    {category.category}
                   </td>
                 </tr>
                 {category.types.map((type) => (
@@ -183,16 +183,16 @@ export function FeatureComparisionTable({planFeatures}) {
                     key={type}
                     className="hover:bg-gray-50 transition duration-200 ease-in-out border-b border-gray-200"
                   >
-                    <td className="pl-8 py-3 px-4">{type}</td>
+                    <td className="pl-8 py-1.5 px-2 sm:py-3 sm:px-4">{type}</td>
                     {planFeatures.plans.map((plan) => (
                       <td
                         key={`${plan.name}-${type}`}
-                        className="text-center py-2 px-4"
+                        className="text-center py-1 px-2 sm:py-2 sm:px-4"
                       >
                         {
-                            planFeatures.availableTypes[plan.name].includes(type) 
-                                ? <span className="mx-auto text-green-500">&#10004;</span>
-                                : <span className="mx-auto text-red-500">&#10006;</span>
+                          planFeatures.availableTypes[plan.name].includes(type) 
+                            ? <span className="mx-auto text-green-500">&#10004;</span>
+                            : <span className="mx-auto text-red-500">&#10006;</span>
                         }
                       </td>
                     ))}
